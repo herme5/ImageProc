3e1922f8506ca1cb5e3400d505f35bd56eed0521
Andrea Ruffino <andrea.ruffino@hotmail.fr>
2023-08-17 03:50:29 +0200

    Implement BitmapData util method

--- ImageProcTests/ImageTests.swift
+++ ImageProcTests/ImageTests.swift
@@ -14,37 +14,35 @@ final class ImageTests: XCTestCase {
     var appBundle: Bundle!
     var shape0: UIImage!
     var shape1: UIImage!
-    var shape2: UIImage!
+    var gradientQuarterImage: UIImage!
+    var notSoBlueImage: UIImage!
+    var smallGradientImage: UIImage!
     var color0: UIColor!
     var color1: UIColor!
     var color2: UIColor!
     var shape0ciImage: CIImage!
 
-    let iterationCount = 10
-
-    func repeated(_ body: () -> Void) {
-        for _ in 0 ..< iterationCount {
-            body()
-        }
-    }
-
     override func setUpWithError() throws {
         appBundle = Bundle(for: ImageTests.self)
         shape0 = UIImage(named: "splash-rounded-100", in: appBundle, with: nil)
         shape1 = UIImage(named: "splash-square-100", in: appBundle, with: nil)
-        shape2 = UIImage(named: "splash-square-line-100", in: appBundle, with: nil)
+        gradientQuarterImage = UIImage(named: "gradient-quarter-100", in: appBundle, with: nil)
+        notSoBlueImage = UIImage(named: "not-so-blue-square-100", in: appBundle, with: nil)
+        smallGradientImage = UIImage(named: "small-gradient-4", in: appBundle, with: nil)
         color0 = UIColor.systemIndigo
         color1 = UIColor.systemPink
         color2 = UIColor.systemTeal
+
+        // This CIImage will be used for initializion
+        // A UIImage initialized this way cannot use some features of this lib.
         shape0ciImage = CIImage(data: shape0.pngData()!)
         shape0 = shape0.withBaselineOffset(fromBottom: 10.0)
     }
 
     func testImages() throws {
-        XCTAssertNotNil(shape0.colorized(with: color0, method: .basic))
-        XCTAssertNotNil(shape0.colorized(with: color0, method: .concurrent))
-        XCTAssertNotNil(shape0.expand(bySize: 1, method: .basic))
-        XCTAssertNotNil(shape0.expand(bySize: 1, method: .concurrent))
+        // These tests cover nominal cases
+        XCTAssertNotNil(shape0.colorized(with: color0))
+        XCTAssertNotNil(shape0.expanded(bySize: 1))
         XCTAssertNotNil(shape0.stroked(with: color0, size: 1))
         XCTAssertNotNil(shape0.smoothened(by: 1, sizeKept: true))
         XCTAssertNotNil(shape0.smoothened(by: 1, sizeKept: false))
@@ -63,38 +61,30 @@ final class ImageTests: XCTestCase {
     }
 
     func testOperationBoundaries() throws {
-        // These cases should fails, but the error handling strategy is to be refined.
-        /*
+        // These cases should trigger an error
+        // For compatibility it just prints warning and returns the original images
         let image = UIImage(ciImage: shape0ciImage)
-        XCTAssertNil(image.colorized(with: color0, method: .basic))
-        XCTAssertNil(image.colorized(with: color0, method: .concurrent))
-        XCTAssertNil(image.expand(bySize: 1, method: .basic))
-        XCTAssertNil(image.expand(bySize: 1, method: .concurrent))
-        XCTAssertNil(image.stroked(with: color0, size: 1))
-        XCTAssertNil(image.smoothened(by: 1, sizeKept: true))
-        XCTAssertNil(image.smoothened(by: 1, sizeKept: false))
-        XCTAssertNil(image.scaled(to: shape0.sizeInPixel / 2))
-        XCTAssertNil(image.scaledWidth(to: 10, keepAspectRatio: true))
-        XCTAssertNil(image.scaledWidth(to: 10, keepAspectRatio: false))
-        XCTAssertNil(image.scaledHeight(to: 10, keepAspectRatio: true))
-        XCTAssertNil(image.scaledHeight(to: 10, keepAspectRatio: false))
-        XCTAssertNil(image.cropped(to: CGRect(origin: .zero, size: shape0.sizeInPixel * 2)))
-        XCTAssertNil(image.cropped(to: CGRect.zero))
-        XCTAssertNil(image.rotated(by: 60))
-        XCTAssertNil(image.flippedHorizontally())
-        XCTAssertNil(image.flippedVertically())
-        XCTAssertNil(image.drawnAbove(image: shape1))
-        XCTAssertNil(image.drawnUnder(image: shape1))
-         */
-        
-        XCTAssertNotNil(shape0
-            .colorized(with: UIColor(ciColor: CIColor(string: "0.0 0.0 0.0 0.0")),
-                       method: .basic))
-        XCTAssertNotNil(shape0
-            .colorized(with: UIColor(ciColor: CIColor(string: "0.0 0.0 0.0 0.0")),
-                       method: .basic))
-        XCTAssertNotNil(shape0.expand(bySize: 1, method: .basic))
-        XCTAssertNotNil(shape0.expand(bySize: 1, method: .concurrent))
+        XCTAssertNotNil(image.colorized(with: color0))
+        XCTAssertNotNil(image.expanded(bySize: 1))
+        XCTAssertNotNil(image.stroked(with: color0, size: 1))
+        XCTAssertNotNil(image.smoothened(by: 1, sizeKept: true))
+        XCTAssertNotNil(image.smoothened(by: 1, sizeKept: false))
+        XCTAssertNotNil(image.scaled(to: shape0.sizeInPixel / 2))
+        XCTAssertNotNil(image.scaledWidth(to: 10, keepAspectRatio: true))
+        XCTAssertNotNil(image.scaledWidth(to: 10, keepAspectRatio: false))
+        XCTAssertNotNil(image.scaledHeight(to: 10, keepAspectRatio: true))
+        XCTAssertNotNil(image.scaledHeight(to: 10, keepAspectRatio: false))
+        XCTAssertNotNil(image.cropped(to: CGRect(origin: .zero, size: shape0.sizeInPixel * 2)))
+        XCTAssertNotNil(image.cropped(to: CGRect.zero))
+        XCTAssertNotNil(image.rotated(by: 60))
+        XCTAssertNotNil(image.flippedHorizontally())
+        XCTAssertNotNil(image.flippedVertically())
+        XCTAssertNotNil(image.drawnAbove(image: shape1))
+        XCTAssertNotNil(image.drawnUnder(image: shape1))
+
+        // These tests try to enhance coverage (unexpected values, specific cases)
+        XCTAssertNotNil(shape0.colorized(with: UIColor(ciColor: CIColor(string: "0.0 0.0 0.0 0.0"))))
+        XCTAssertNotNil(shape0.expanded(bySize: 1))
         XCTAssertNotNil(shape0.stroked(with: color0, size: 1))
         XCTAssertNotNil(shape0.smoothened(by: 1, sizeKept: true))
         XCTAssertNotNil(shape0.smoothened(by: 1, sizeKept: false))
@@ -112,44 +102,56 @@ final class ImageTests: XCTestCase {
         XCTAssertNotNil(shape0.drawnUnder(image: shape1))
     }
 
-    func testColorizedBasic() throws {
-        measure {
-            repeated {
-                _ = shape0.colorized(with: color0, method: .basic)
-            }
+    func testBitmapProcessing() throws {
+
+        // Exclusion with the same image should result in full transparent image
+        let density0 = shape0.alphaMasked(with: shape0).opaquePixelDensity
+        XCTAssertNotNil(density0)
+        XCTAssertEqual(density0!, 0.0)
+
+        // This image is a fully opaque
+        let density1 = notSoBlueImage.opaquePixelDensity
+        XCTAssertNotNil(density1)
+        XCTAssertEqual(density1!, 1.0)
+
+        // This image is a square taking quarter of the total size
+        let density2 = gradientQuarterImage.opaquePixelDensity
+        XCTAssertNotNil(density2)
+        XCTAssertEqual(density2!, 0.25, accuracy: 0.001)
+
+        // This is image is constructed with apparent colors in the corners
+        let img = smallGradientImage.cgImage!
+        let entries = [
+            CGImage.PixelCoordinate.topLeft(in: img): "#FF0000",
+            CGImage.PixelCoordinate.topRight(in: img): "#FFFF00",
+            CGImage.PixelCoordinate.bottomLeft(in: img): "#FF00FF",
+            CGImage.PixelCoordinate.bottomRight(in: img): "#FFFFFF"
+        ]
+        for entry in entries {
+            let color = img.color(at: entry.key)
+            XCTAssertNotNil(color)
+            XCTAssertEqual(color!.hexCode, entry.value)
         }
     }
 
-    func testColorizedConcurrent() throws {
-        measure {
-            repeated {
-                _ = shape0.colorized(with: color0, method: .concurrent)
-            }
-        }
+    func testColorizedWithCiFilter() throws {
+        // CIFilter colorization is the default unless UIImage.useMetalColorizationMethod() is called before.
+        measure { _ = shape0.colorized(with: color0) }
     }
 
-    func testExpandBasic() throws {
-        measure {
-            repeated {
-                _ = shape0.expand(bySize: 20, method: .basic)
-            }
-        }
+    func testColorizedWithMetal() throws {
+        // Metal colorization is selected when UIImage.useMetalColorizationMethod() is called.
+        UIImage.useMetalColorizationMethod()
+        measure { _ = shape0.colorized(with: color0) }
     }
 
-    func testExpandConcurrent() throws {
-        measure {
-            repeated {
-                _ = shape0.expand(bySize: 20, method: .concurrent)
-            }
-        }
+    func testExpand() throws {
+        measure { _ = shape0.expanded(bySize: 20) }
     }
-    
+
     func testStroked() throws {
+        // Keep in mind that regarding of the previous test CIFilter or Metal colorization is called.
         let color = UIColor.black
-        measure {
-            repeated {
-                _ = shape0.stroked(with: color, size: 20)
-            }
-        }
+        measure { _ = shape0.stroked(with: color, size: 20) }
     }
 }
